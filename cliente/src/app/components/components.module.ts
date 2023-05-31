09fdda048b783e9f941fd34baa332861b38ade11
U2D-8Bits <u2d.8bits@gmail.com>
2023-05-31 10:00:55 -0500

    Verificacion npm run build

--- cliente/src/app/components/components.module.ts
+++ cliente/src/app/components/components.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MateriaComponent } from './materia/materia.component';
 import { ApuntesComponent } from './apuntes/apuntes.component';
 import { TareasComponent } from './tareas/tareas.component';
 import { HorarioComponent } from './horario/horario.component';
 import { RegistroComponent } from './registro/registro.component';
+import { MateriaComponent } from './Materia/materia.component';
 
 
 
 @NgModule({
   declarations: [
-    MateriaComponent,
     ApuntesComponent,
     TareasComponent,
     HorarioComponent,
-    RegistroComponent
+    RegistroComponent,
+    MateriaComponent
   ],
   imports: [
     CommonModule,
