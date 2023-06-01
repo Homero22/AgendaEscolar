b0afc97a265d7869b6851d3f34289387d69e7c64
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2023-05-31 22:17:10 -0500

    Validaciones de formulario y botón

--- cliente/src/app/core/conferencias.service.ts
+++ cliente/src/app/core/conferencias.service.ts
@@ -1,21 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { conferenciasModel, dataConferencias } from './models/conferencias';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ConferenciasService {
 
-  private url = 'https://26.241.69.100:4000/conferencias';
-
-
 
   constructor(private http: HttpClient) { }
 
-  postConferencias(conferencias: dataConferencias) {
-    console.log("data en el servicio",conferencias);
-    return this.http.post<conferenciasModel>(`${this.url}`, conferencias);
-  }
 }
