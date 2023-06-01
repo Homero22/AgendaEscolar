cba7f88f1579d1760f7e46ea249b771a072d75c4
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2023-05-31 21:27:58 -0500

    Formulario de loguin creado y en html

--- cliente/src/app/pages/home/home.component.ts
+++ cliente/src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +8,21 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  hide = true;
-  email!: FormControl;
+  hide = true;          //para el password
+  email!: FormControl;  //para el email
+  loginForm!: FormGroup;
 
 private destroy$ = new Subject<any>();
 
 constructor(
-  // public fb: FormBuilder,
+  public fb: FormBuilder,
 ) {
   this.email = new FormControl('', [Validators.required, Validators.email]);
+
+  this.loginForm = this.fb.group({
+    email: this.email,
+    password: ['', Validators.required]
+  });
 }
 
 ngOnInit(): void {}
@@ -27,6 +33,15 @@ getErrorMessage() {
 
   return this.email.hasError('email') ? 'Correo ingresado no valido' : '';
 }
+  submitForm() {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      console.log(formData.email);
+      console.log(formData.password);
+      // Aquí puedes hacer lo que necesites con los valores de los inputs
+    }
+  }
+
 
 ngOnDestroy(): void {
   this.destroy$.next({});
