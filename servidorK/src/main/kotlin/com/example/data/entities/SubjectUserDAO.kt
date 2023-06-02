2f25c2dadc4de0e990f35308b47f463873b77d58
Homero <homero6834@gmail.com>
2023-06-02 15:35:32 -0500

    com

--- servidorK/src/main/kotlin/com/example/data/entities/SubjectUserDAO.kt
+++ servidorK/src/main/kotlin/com/example/data/entities/SubjectUserDAO.kt
@@ -8,7 +8,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 
 object MateriaUsuario : IntIdTable("tb_usuario_materia") {
     val idUsuario = integer("int_usuario_id").uniqueIndex()
-    val idMateria = integer("int_materia_id").uniqueIndex()
+    val idMateria = integer("int_materia_id").references(Subjects.id)
     val materiaAcro = varchar("str_materia_acro", 255)
     val materiaColor = varchar("str_materia_color", 255)
     val profesorNombre = varchar("str_nombre_profesor", 255)
