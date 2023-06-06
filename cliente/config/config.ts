6464a80232f4ce698e825c32351e4931dc4d7883
U2D-8Bits <u2d.8bits@gmail.com>
2023-06-05 20:05:17 -0500

    Actualizacion Frontend

--- cliente/config/config.ts
+++ cliente/config/config.ts
@@ -3,6 +3,7 @@
 import { environment } from "src/app/environments/environments";
 
 export default {
-  // URL_BASE: environment.url,
+  URL_BASE: environment.url,
   URL_BASE_PATH: environment.baseUrl,
+  URL_API_BASE: environment.urlApi,
 };
