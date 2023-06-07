5288a1c35aa7beed2d77e399041a9562029d112f
U2D-8Bits <u2d.8bits@gmail.com>
2023-06-07 10:09:33 -0500

    Diseño pagina 404

--- cliente/src/app/shared/pages/error404/error404.component.ts
+++ cliente/src/app/shared/pages/error404/error404.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-error404',
   templateUrl: './error404.component.html',
-  styles: [
-  ]
+  styleUrls: ['./error404.component.css']
 })
 export class Error404Component {
 
