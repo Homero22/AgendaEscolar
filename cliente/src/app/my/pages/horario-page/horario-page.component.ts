6236ba7e02507cde7b7579f3adee9b298f7ae32d
U2D-8Bits <u2d.8bits@gmail.com>
2023-06-07 23:45:18 -0500

    Actualizacion apartado Visual

--- cliente/src/app/my/pages/horario-page/horario-page.component.ts
+++ cliente/src/app/my/pages/horario-page/horario-page.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-horario-page',
   templateUrl: './horario-page.component.html',
-  styles: [
-  ]
+styleUrls: ['./horario-page.component.css']
 })
-export class HorarioPageComponent {
+  export class HorarioPageComponent {
+
+    ngOnInit(): void {
+    }
+
+
+    assignFunction(day: string, hour: string) {
+      console.log(`Asignar función para ${day} a las ${hour}`);
+    }
 
 }
