997ba23e1b2c3767533dc93b8cb4a8a4defc84b9
U2D-8Bits <u2d.8bits@gmail.com>
2023-06-16 00:49:40 -0500

    Update Materia

--- cliente/src/app/components/ajustes-materia/agregar-materia/agregar-materia.component.ts
+++ cliente/src/app/components/ajustes-materia/agregar-materia/agregar-materia.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MateriaService } from 'src/app/core/services/materia.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-materia',
@@ -17,24 +19,25 @@ export class AgregarMateriaComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private srvMateria: MateriaService
   ) {
     this.myForm = this.fb.group({
-      materia: [
+      nombre: [
         '',
         [Validators.required, Validators.pattern("^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\\s]*$")]
       ],
-      acronimo: [
+      materiaAcro: [
         '',
         [
           Validators.required,
           Validators.pattern(/^[A-Z]{3}$/)
         ]
       ],
-      color: [
+      materiaColor: [
         '',
        [ Validators.pattern(/^#[0-9A-F]{6}$/i)]
       ],
-      profesor: [
+      profesorNombre: [
         '',
         [Validators.required,
         Validators.pattern("^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\\s]*$")
@@ -60,6 +63,58 @@ export class AgregarMateriaComponent implements OnInit {
 
   saveMateria(){
     console.log("Valor que llega al Form de Materia =>",this.myForm.value);
+
+    const sendMateriaData = this.myForm.value;
+
+    Swal.fire({
+      title:'Esta seguro de añadir esta Materia?',
+      showDenyButton:true,
+      confirmButtonText:'Agregar',
+      denyButtonText:'Cancelar'
+    }).then((result)=>{
+      if(result.isConfirmed){
+        this.srvMateria.postMateria(sendMateriaData)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (data)=>{
+            if(data.status){
+              Swal.fire({
+                title:'Materia agregada con éxito!',
+                icon:'success',
+                showConfirmButton: false,
+                timer: 1500
+              })
+              console.log("Materia agregada con éxito =>",data);
+            }else{
+              Swal.fire({
+                title:'Error al agregar Materia!',
+                icon:'error',
+                showConfirmButton: false,
+                timer: 1500
+              })
+              console.log("Error al agregar proveedor =>", data);
+            }
+            setTimeout(() => {
+              Swal.close();
+            }, 3000);
+          },
+          error: (error)=>{
+            Swal.fire({
+              title:'Error al agregar Materia!',
+              icon:'error',
+              showConfirmButton: false,
+              timer: 1500
+            })
+            console.log("Error al agregar proveedor =>", error);
+          },
+          complete: ()=>{
+            console.log("Petición completa!");
+            this.myForm.reset();
+          }
+        })
+      }
+    })
+
   }
 
   ngOnDestroy(): void {
--- cliente/src/app/core/models/materia.ts
+++ cliente/src/app/core/models/materia.ts
@@ -8,16 +8,36 @@ export interface ShowMateriaModel {
 export interface MateriaModel {
     id: number;
     nombre: string;
+    MateriaAcro: string;
+    materiaColor: string;
+    profesorNombre: string;
 }
 
-export interface MateriaModel {
-    status: boolean;
-    message: string;
-    body: MateriaData;
-}
+// export interface MateriaModel {
+//     status: boolean;
+//     message: string;
+//     body: MateriaData;
+// }
 
 export interface MateriaData {
-    nombre: string;
+  id: number;
+  nombre: string;
+  materiaAcro: string;
+  materiaColor: string;
+  profesorNombre: string;
 }
 
+// --------------------- Agregar Materia ---------------------
 
+export interface addMateriaModel{
+  status: boolean;
+  message: string;
+  body: addMateriaData;
+}
+
+export interface addMateriaData{
+  nombre: string;
+  materiaAcro: string;
+  materiaColor: string;
+  profesorNombre: string;
+}
