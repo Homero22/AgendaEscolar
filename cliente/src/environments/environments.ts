0fbcba4c501e8acc47dad8b6d60340cfe03b7794
U2D-8Bits <u2d.8bits@gmail.com>
2023-06-16 08:33:03 -0500

    update AgregarMateria

--- cliente/src/environments/environments.ts
+++ cliente/src/environments/environments.ts
@@ -3,6 +3,4 @@ export const environment = {
   production: false,
   baseUrl: baseUrl,
   url: 'https://localhost:4200/' + baseUrl,
-  // urlApi: 'http://127.0.0.1:8002/',
-  urlApi: 'http://26.241.69.100:8002/'
 };
