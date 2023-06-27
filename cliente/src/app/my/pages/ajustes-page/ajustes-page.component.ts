58487d43d2955ddd94fb9debf911122fdae507da
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2023-06-27 08:14:49 -0500

    css de mapa listo

--- cliente/src/app/my/pages/ajustes-page/ajustes-page.component.ts
+++ cliente/src/app/my/pages/ajustes-page/ajustes-page.component.ts
@@ -47,87 +47,162 @@ export class AjustesPageComponent {
     },
   ]
 
+  // ngOnInit(): void {
+  //   this.getUsuariosPais();
+  //   setTimeout(() => {
+  //     const svg = document.getElementById('mapa');
+  //     const paths = svg?.getElementsByTagName('path');
+
+  //     const maxUsuarios = Math.max(...this.datos.map(dato => dato.usuarios));
+  //     const minUsuarios = Math.min(...this.datos.map(dato => dato.usuarios));
+
+  //     for (const dato of this.datos) {
+  //       const path = document.getElementById(dato.acronimo);
+
+  //       path?.addEventListener('mouseenter', () => {
+  //         this.mostrarInfoPais(dato);
+  //       });
+
+  //       path?.addEventListener('mouseleave', () => {
+  //         this.ocultarInfoPais();
+  //       });
+
+  //       // Calcula el color basado en la cantidad de usuarios
+  //       const intensidad = this.calcularIntensidad(dato.usuarios, minUsuarios, maxUsuarios);
+  //       const color = this.calcularColor(intensidad);
+
+  //       // Aplica el color al elemento <path>
+  //       if (path) {
+  //         path.style.fill = color;
+  //       }
+  //     }
+  //   }, 100);
+  // }
+
+  // calcularIntensidad(usuarios: number, minUsuarios: number, maxUsuarios: number): number {
+  //   const porcentaje = (usuarios - minUsuarios) / (maxUsuarios - minUsuarios);
+  //   return Math.round(porcentaje * 100);
+  // }
+
+  // calcularColor(intensidad: number): string {
+  //   const color = Math.round(255 - intensidad * 2.55);
+  //   return `rgb(0, 0, ${color})`;
+  // }
+
+  // mostrarInfoPais(pais: { nombre: string, usuarios: number }): void {
+  //   this.paisSeleccionado = pais;
+  // }
+
+  // ocultarInfoPais(): void {
+  //   this.paisSeleccionado = null;
+  // }
+
+  // getUsuariosPais(){
+  //   this.srvReportes.getUsuariosPais()
+  //   .pipe(takeUntil(this.destroy$))
+  //   .subscribe({
+  //     next:(reporteData)=>{
+  //       if(reporteData.body){
+  //         this.srvReportes.datos = reporteData.body;
+  //         console.log("Valor de datos =>",this.srvReportes.datos);
+  //       }else{
+  //         console.log("No hay datos");
+  //       }
+  //     }
+  //   });
+  // }
+
+  // mostrarTooltip(pais: any, event: MouseEvent): void {
+  //   const container = document.getElementById('mapa-container');
+  //   if (container) {
+  //     const containerRect = container.getBoundingClientRect();
+  //     const tooltipLeft = event.clientX - containerRect.left + window.pageXOffset;
+  //     const tooltipTop = event.clientY - containerRect.top + window.pageYOffset;
+
+  //     this.tooltipText = `Nombre: ${pais.nombre} \nUsuarios: ${pais.usuarios}`;
+  //     container.style.setProperty('--tooltip-left', `${tooltipLeft}px`);
+  //     container.style.setProperty('--tooltip-top', `${tooltipTop}px`);
+  //   }
+  // }
+
+  // ocultarTooltip(): void {
+  //   this.tooltipText = undefined;
+  // }
+
   ngOnInit(): void {
     this.getUsuariosPais();
     setTimeout(() => {
-      const svg = document.getElementById('mapa');
-      const paths = svg?.getElementsByTagName('path');
-
-      const maxUsuarios = Math.max(...this.datos.map(dato => dato.usuarios));
-      const minUsuarios = Math.min(...this.datos.map(dato => dato.usuarios));
-
-      for (const dato of this.datos) {
-        const path = document.getElementById(dato.acronimo);
-
-        path?.addEventListener('mouseenter', () => {
-          this.mostrarInfoPais(dato);
-        });
-
-        path?.addEventListener('mouseleave', () => {
-          this.ocultarInfoPais();
-        });
-
-        // Calcula el color basado en la cantidad de usuarios
-        const intensidad = this.calcularIntensidad(dato.usuarios, minUsuarios, maxUsuarios);
-        const color = this.calcularColor(intensidad);
-
-        // Aplica el color al elemento <path>
-        if (path) {
-          path.style.fill = color;
-        }
-      }
+      this.actualizarColores();
+      this.registrarEventos();
     }, 100);
   }
-
+  
+  private actualizarColores(): void {
+    const maxUsuarios = Math.max(...this.datos.map(dato => dato.usuarios));
+    const minUsuarios = Math.min(...this.datos.map(dato => dato.usuarios));
+  
+    for (const dato of this.datos) {
+      const path = document.getElementById(dato.acronimo);
+      if (path) {
+        const intensidad = this.calcularIntensidad(dato.usuarios, minUsuarios, maxUsuarios);
+        const color = this.calcularColor(intensidad);
+        path.style.fill = color;
+      }
+    }
+  }
+  
+  private registrarEventos(): void {
+    const svg = document.getElementById('mapa');
+    const paths = svg?.getElementsByTagName('path');
+  
+    for (const dato of this.datos) {
+      const path = document.getElementById(dato.acronimo);
+      if (path) {
+        path.addEventListener('mouseenter', () => {
+          this.mostrarInfoPais(dato);
+        });
+  
+        path.addEventListener('mouseleave', () => {
+          this.ocultarInfoPais();
+        });
+      }
+    }
+  }
+  
   calcularIntensidad(usuarios: number, minUsuarios: number, maxUsuarios: number): number {
     const porcentaje = (usuarios - minUsuarios) / (maxUsuarios - minUsuarios);
     return Math.round(porcentaje * 100);
   }
-
+  
   calcularColor(intensidad: number): string {
     const color = Math.round(255 - intensidad * 2.55);
     return `rgb(0, 0, ${color})`;
   }
-
+  
   mostrarInfoPais(pais: { nombre: string, usuarios: number }): void {
     this.paisSeleccionado = pais;
   }
-
+  
   ocultarInfoPais(): void {
     this.paisSeleccionado = null;
   }
-
-  getUsuariosPais(){
+  
+  getUsuariosPais(): void {
     this.srvReportes.getUsuariosPais()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next:(reporteData)=>{
-        if(reporteData.body){
-          this.srvReportes.datos = reporteData.body;
-          console.log("Valor de datos =>",this.srvReportes.datos);
-        }else{
-          console.log("No hay datos");
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (reporteData) => {
+          if (reporteData.body) {
+            this.srvReportes.datos = reporteData.body;
+            console.log("Valor de datos =>", this.srvReportes.datos);
+            this.actualizarColores();
+            this.registrarEventos();
+          } else {
+            console.log("No hay datos");
+          }
         }
-      }
-    });
+      });
   }
   
-  mostrarTooltip(pais: any, event: MouseEvent): void {
-    const container = document.getElementById('mapa-container');
-    if (container) {
-      const containerRect = container.getBoundingClientRect();
-      const tooltipLeft = event.clientX - containerRect.left + window.pageXOffset;
-      const tooltipTop = event.clientY - containerRect.top + window.pageYOffset;
-
-      this.tooltipText = `Nombre: ${pais.nombre} \nUsuarios: ${pais.usuarios}`;
-      container.style.setProperty('--tooltip-left', `${tooltipLeft}px`);
-      container.style.setProperty('--tooltip-top', `${tooltipTop}px`);
-    }
-  }
-
-  ocultarTooltip(): void {
-    this.tooltipText = undefined;
-  }
-
 
 }
