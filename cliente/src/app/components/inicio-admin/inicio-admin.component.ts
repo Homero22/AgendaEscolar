f390c411fbdd7299d16e4435fd0200467c17b53f
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2023-07-01 14:33:49 -0500

    servicios para inicio admin

--- cliente/src/app/components/inicio-admin/inicio-admin.component.ts
+++ cliente/src/app/components/inicio-admin/inicio-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js'; 
+import { takeUntil, Subject } from 'rxjs';
+import { InicioAdminService } from 'src/app/core/services/inicio-admin.service';
 
 @Component({
   selector: 'app-inicio-admin',
@@ -7,4 +10,44 @@ import { Component } from '@angular/core';
 })
 export class InicioAdminComponent {
 
+  private destroy$ = new Subject<any>();
+
+  constructor(
+    public srvInicioAdmin: InicioAdminService
+  ){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    // this.getNumCard()
+  }
+
+  getNumCard(){
+    this.srvInicioAdmin.getUsersTotal()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (res)=>{
+        console.log("cantidad de usuarios ",res)
+        this.srvInicioAdmin.usuariosT = res.body
+      }
+    })
+
+    this.srvInicioAdmin.getMateriasTotales()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (value) =>{
+          console.log("cantidad de materias ",value)
+          this.srvInicioAdmin.materiasT = value.body
+        },
+    })
+
+    this.srvInicioAdmin.getAdminTotales()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (value) =>{
+          console.log("cantidad de administradores ",value)
+          this.srvInicioAdmin.adminsT = value.body
+      },
+    })
+  }
 }
---
+++ cliente/src/app/core/services/inicio-admin.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import config from 'config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InicioAdminService {
+
+  private urlApi_userTotal: string = config.URL_API_BASE + "users/total"
+  private urlApi_materiasTotal: string = config.URL_API_BASE + "subjects/total"
+  private urlApi_anios: string = config.URL_API_BASE + "anios"
+  private urlApi_adminT: string = config.URL_API_BASE + ""
+  private urlApi_userxmes: string = config.URL_API_BASE + ""
+
+  constructor(private http: HttpClient) { }
+
+  usuariosT!: number;
+  materiasT!: number;
+  adminsT!: number;
+  anios!: number;
+
+  //-----------Para obtener los valores de los cards------------
+
+
+  getUsersTotal(){
+    return this.http.get<any>(`${this.urlApi_userTotal}`,
+    {
+      withCredentials: true
+    }
+    )
+  }
+
+  getMateriasTotales(){
+    return this.http.get<any>(`${this.urlApi_materiasTotal}`,
+    {
+      withCredentials: true
+    }
+    )
+  }
+
+  getAdminTotales(){
+    return this.http.get<any>(`${this.urlApi_adminT}`,
+    {
+      withCredentials: true
+    })  
+  }
+
+  //------------------para el gráfico user-tiempo--------------------
+
+  getAniosDisponible(){
+    return this.http.get<any>(`${this.urlApi_anios}`,
+    {
+      withCredentials: true
+    })
+  }
+
+  getUserXMes(anio: number){
+    return this.http.get<any>(`${this.urlApi_userxmes}/${anio}`,
+    {
+      withCredentials: true
+    })
+  }
+
+
+}
