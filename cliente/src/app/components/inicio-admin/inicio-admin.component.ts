e99635521e367a0d04008418e38dfe3d79361025
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2023-07-01 19:36:52 -0500

    ya me salio un gráfico, sin datos

--- cliente/src/app/components/inicio-admin/inicio-admin.component.ts
+++ cliente/src/app/components/inicio-admin/inicio-admin.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { Chart } from 'chart.js'; 
+// import { Chart } from 'chart.js'; 
 import { takeUntil, Subject } from 'rxjs';
 import { InicioAdminService } from 'src/app/core/services/inicio-admin.service';
-// import * as Chart from 'chart.js';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-inicio-admin',
@@ -18,59 +18,25 @@ export class InicioAdminComponent {
     public srvInicioAdmin: InicioAdminService
   ){}
 
-  chart=[]
-  maxCantidad!: number
-  minCantidad!: number
-  meses: string[] = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
-
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.getNumCard();
     this.getDatosGrafico();
-    // this.createBarChart();
+    // this.crearbarchart();
 
-    // this.rangos();
-   
   }
 
-  // ngAfterViewInit() {
-  //   if (this.barChartCanvas) {
-  //     this.createBarChart();
-  //   }
-  // }
-
-  getNumCard(){
-    this.srvInicioAdmin.getUsersTotal()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next: (res)=>{
-        console.log("cantidad de usuarios ",res)
-        this.srvInicioAdmin.usuariosT = res.body
-      }
-    })
-
-    this.srvInicioAdmin.getMateriasTotales()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next: (value) =>{
-          console.log("cantidad de materias ",value)
-          this.srvInicioAdmin.materiasT = value.body
-        },
-    })
-
-    this.srvInicioAdmin.getAdminTotales()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next: (value) =>{
-          console.log("cantidad de administradores ",value)
-          this.srvInicioAdmin.adminsT = value.body
-      },
-    })
+  ngAfterViewInit() {
+    // if (this.srvInicioAdmin.datos) {
+    //   console.log("existe datos")
+    //   setTimeout(() => {
+    //     this.createBarChart();
+    //   }, 100);
+    // }
+    // this.crearbarchart();
   }
+  
 
   getDatosGrafico(){
-    // const anio = '2023'
     const fechaActual = new Date();
     const anio = fechaActual.getFullYear(); 
 
@@ -84,7 +50,9 @@ export class InicioAdminComponent {
     })
 
     this.getcontenido(anio)
-    this.createBarChart();
+    // this.createBarChart();
+    // this.crearbarchart();
+
 
 }
 
@@ -95,61 +63,15 @@ getcontenido(anio: number){
       next: (value) =>{
           console.log("datos del gráfico ",value)
           this.srvInicioAdmin.datos = value.body
+          this.crearbarchart()
       },
     })
 }
 
-rangos(){
-  this.maxCantidad = Math.max(...this.srvInicioAdmin.datos.map(dato => dato.cantidad));
-  this.minCantidad = Math.min(...this.srvInicioAdmin.datos.map(dato => dato.cantidad));
-
-}
-
-// createBarChart() {
-//   // const datos = [
-//   //   { mes: 'Junio', cantidad: 7 },
-//   //   { mes: 'Julio', cantidad: 3 }
-//   // ];
-
-//   const labels = this.srvInicioAdmin.datos.map(dato => dato.mes);
-//   const valores = this.srvInicioAdmin.datos.map(dato => dato.cantidad);
-
-//   // const canvas: HTMLCanvasElement = document.getElementById('barChart') as HTMLCanvasElement;
-//   // const ctx = canvas.getContext('2d');
-
-//   const ctx = this.barChartCanvas?.nativeElement.getContext('2d');
-
-//   new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//       labels: labels,
-//       datasets: [{
-//         label: 'Cantidad',
-//         data: valores,
-//         backgroundColor: 'rgba(0, 123, 255, 0.5)', // Color de las barras
-//         borderColor: 'rgba(0, 123, 255, 1)', // Color del borde de las barras
-//         borderWidth: 1 // Grosor del borde de las barras
-//       }]
-//     },
-//     options: {
-//       responsive: true,
-//       scales: {
-//         y: {
-//           beginAtZero: true
-//         }
-//       }
-//     }
-//   });
-// }
-
 createBarChart() {
-  const datos = [
-    { mes: 'Junio', cantidad: 7 },
-    { mes: 'Julio', cantidad: 3 }
-  ];
-
-  const labels = datos.map(dato => dato.mes);
-  const valores = datos.map(dato => dato.cantidad);
+/*
+  const labels = this.srvInicioAdmin.datos.map(dato => dato.mes);
+  const valores = this.srvInicioAdmin.datos.map(dato => dato.cantidad);
 
   const ctx = this.barChartCanvas?.nativeElement.getContext('2d');
 
@@ -174,5 +96,107 @@ createBarChart() {
       }
     }
   });
+  */
+  const datos = [
+  { mes: 'Junio', cantidad: 7 },
+  { mes: 'Julio', cantidad: 3 }
+];
+
+const chartContainer = document.querySelector('.chart-container');
+const chartBar = document.querySelector('.chart-bar');
+
+const maxValor = Math.max(...datos.map(dato => dato.cantidad));
+
+datos.forEach(dato => {
+  const porcentaje = (dato.cantidad / maxValor) * 100;
+  const barra = document.createElement('div');
+  barra.classList.add('barra');
+  barra.style.height = porcentaje + '%';
+  barra.innerText = dato.cantidad.toString();
+  chartBar?.appendChild(barra);
+});
+
 }
+
+
+getNumCard(){
+  this.srvInicioAdmin.getUsersTotal()
+  .pipe(takeUntil(this.destroy$))
+  .subscribe({
+    next: (res)=>{
+      console.log("cantidad de usuarios ",res)
+      this.srvInicioAdmin.usuariosT = res.body
+    }
+  })
+
+  this.srvInicioAdmin.getMateriasTotales()
+  .pipe(takeUntil(this.destroy$))
+  .subscribe({
+    next: (value) =>{
+        console.log("cantidad de materias ",value)
+        this.srvInicioAdmin.materiasT = value.body
+      },
+  })
+
+  this.srvInicioAdmin.getAdminTotales()
+  .pipe(takeUntil(this.destroy$))
+  .subscribe({
+    next: (value) =>{
+        console.log("cantidad de administradores ",value)
+        this.srvInicioAdmin.adminsT = value.body
+    },
+  })
+}
+
+crearbarchart(){
+  console.log("entro a crearbarchart")
+    console.log("entro")
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    console.log("canvas ",canvas)
+    if (canvas instanceof HTMLCanvasElement) {
+      const ctx = canvas.getContext('2d');
+      console.log("ctx ",ctx)
+      if (ctx) {
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: 'N° Usuarios',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(0, 123, 255, 0.5)',
+                  'rgba(0, 123, 255, 0.5)',
+                  'rgba(0, 123, 255, 0.5)',
+                  'rgba(0, 123, 255, 0.5)',
+                  'rgba(0, 123, 255, 0.5)',
+                  'rgba(0, 123, 255, 0.5)'
+              ],
+              borderColor: [
+                  'rgba(0, 123, 255, 1)',
+                  'rgba(0, 123, 255, 1)',
+                  'rgba(0, 123, 255, 1)',
+                  'rgba(0, 123, 255, 1)',
+                  'rgba(0, 123, 255, 1)',
+                  'rgba(0, 123, 255, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        scales: {
+
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+      });
+    }
+    } else {
+      console.error("No se encontró el elemento canvas con el ID 'myChart'");
+    }
+}
+
+
 }
