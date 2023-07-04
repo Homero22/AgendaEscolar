381740efa236474e7e6245df274c2e048b5fdd7d
U2D-8Bits <u2d.8bits@gmail.com>
2023-07-04 11:27:12 -0500

    uptade miCuenta

--- cliente/src/app/core/services/usuario.service.ts
+++ cliente/src/app/core/services/usuario.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import config from 'config/config';
-import { ShowUsuarioModel, addUsuarioData } from '../models/usuario';
+import { addUsuarioData, updateUsuarioData } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+
+  //Contenido de usuario
+  usuarioData: updateUsuarioData = {
+    id: 0,
+    nombre: "",
+    apellido: "",
+    rol:"",
+    telefono: "",
+    correo: "",
+    contrasena: "",
+    paisId: 0,
+    nivelEstudio: "",
+    fechaCreacion: "",
+    estado: ""
+  }
+
+
 
   //Rutas de los servicios
   private urlApi_Usuarios: string = config.URL_API_BASE + "users";
@@ -19,6 +36,15 @@ export class UsuarioService {
   //listar Usuarios
   getUsuarios(){
     return this.http.get<any>(`${this.urlApi_Usuarios}`,
+      {
+        withCredentials: true
+      });
+  }
+
+  //Obtener un usuario por ID
+
+  getUserByID(idUser: number){
+    return this.http.get<any>(`${this.urlApi_Usuarios}/${idUser}`,
       {
         withCredentials: true
       });
--- cliente/src/app/my/pages/ajustes-page/ajustes-page.component.ts
+++ cliente/src/app/my/pages/ajustes-page/ajustes-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { UsuarioService } from 'src/app/core/services/usuario.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ajustes-page',
@@ -8,11 +10,70 @@ import { Subject } from 'rxjs';
   styleUrls: [ './ajustes-page.component.css'
   ]
 })
-export class AjustesPageComponent {
+export class AjustesPageComponent implements OnInit {
   private destroy$ = new Subject<any>();
 
+  idUser!: any;
+
+  myForm!: FormGroup;
+
+
   constructor(
-  ) { }
+    public srvUsuario: UsuarioService,
+    public fb: FormBuilder
+  ) {
+
+    //Declaramos el formulario
+    this.myForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      telefono: ['', [Validators.required]],
+      correo: ['', [Validators.required]],
+      contrasena: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.idUser = sessionStorage.getItem("id");
+    this.completeInfo();
+  }
+
+  //Funcion para completar la información del usuario
+  completeInfo(){
+    this.getUserByID();
+    //Completamos el formulario con la información del usuari
+
+
+  }
+
+  //Función para obtener la información del usuario
+  getUserByID(){
+    this.srvUsuario.getUserByID(this.idUser)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next:(res)=>{
+        console.log(res);
+        this.srvUsuario.usuarioData = res.body;
+        console.log("Contenido en usuarioData => ",this.srvUsuario.usuarioData);
+        //completamos la informacion del formulario
+        this.myForm.setValue({
+          nombre: this.srvUsuario.usuarioData.nombre,
+          apellido: this.srvUsuario.usuarioData.apellido,
+          telefono: this.srvUsuario.usuarioData.telefono,
+          correo: this.srvUsuario.usuarioData.correo,
+          contrasena: this.srvUsuario.usuarioData.contrasena
+        });
+        console.log("Contenido del Formulario => ",this.myForm.value)
+      },
+      error:(err)=>{
+        console.log(err);
+      },
+      complete:()=>{
+        console.log("complete");
+      }
+    });
+  }
+
 
 ngOnDestroy(): void {
   this.destroy$.next({});
