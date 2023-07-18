93b6ce2a1fcedf6f010fcf1e744e9c804eb6674b
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2023-07-18 01:20:02 -0500

    Usuarios en reportes

--- cliente/src/app/components/usuarios/usuarios.component.ts
+++ cliente/src/app/components/usuarios/usuarios.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { takeUntil, Subject } from 'rxjs';
+import Swal from 'sweetalert2';
+import { UsuarioService } from 'src/app/core/services/usuario.service';
+import { updateUsuarioData } from 'src/app/core/models/usuario';
+import { V } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-usuarios',
@@ -6,5 +13,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent {
+
+  displayedColumns: string[] = ['index', 'nombre', 'correo', 'telefono','fechaCreacion'];
+  dataSource = new MatTableDataSource<updateUsuarioData>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  private destroy$ = new Subject<any>();
+
+  constructor( public srvUsuario: UsuarioService ){}
+
+  ngOnInit(): void {
+    this.getUsuarios();
+  }
+
+  getUsuarios() {
+    this.srvUsuario.getUsuarios()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next:(usuarioData)=>{
+        console.log("usuarioData =>",usuarioData);
+        if(usuarioData.body){
+          this.srvUsuario.usuarios = usuarioData.body;
+          this.dataSource = new MatTableDataSource<updateUsuarioData>(this.srvUsuario.usuarios);
+          this.dataSource.paginator = this.paginator;
+        }else{
+          console.log("No hay datos");
+        }
+      }
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next({});
+    this.destroy$.complete();
+  }
 
 }
